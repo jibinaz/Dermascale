17c75f703336cf65766c8dbf6ed3894d4529cbd6
jibinaz <144548144+jibinaz@users.noreply.github.com>
2024-04-17 16:20:04 +0530

    skingg
    to be completed

---
+++ js/page4.js
@@ -0,0 +1,16 @@
+
+var swiper = new Swiper( '.swiper-container.two', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    effect: 'coverflow',
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflow: {
+        rotate: 0,
+        stretch: 100,
+        depth: 150,
+        modifier: 1.5,
+        slideShadows : false,
+    }
+} );
