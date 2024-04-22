7512fced30e0f483b336af5c4f07fc09f23bdee4
jibinaz <144548144+jibinaz@users.noreply.github.com>
2024-04-22 09:26:03 +0530

    done

---
+++ model/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import joblib
+from pandas import DataFrame
+
+app = Flask(__name__)
+CORS(app)
+loaded_model = joblib.load('est_model.pkl') 
+
+@app.route('/predict', methods=['POST'])
+def receive_data():
+    data = request.get_json()  
+    data = {key: [value] for key, value in data.items()}
+    df = DataFrame(data)  
+    prediction = loaded_model.predict(df)  
+    return jsonify({'prediction': prediction.tolist()})  
+
+if __name__ == '__main__':
+    app.run(debug=True)
